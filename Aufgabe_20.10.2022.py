fbe655b45b7ad846ebc9b18b59bfb65c3e8e8aa0
Stefan <115979341+xSTSNx@users.noreply.github.com>
2022-10-20 14:52:57 +0200

    Update Aufgabe_20.10.2022.py

--- Aufgabe_20.10.2022.py
+++ Aufgabe_20.10.2022.py
@@ -1,13 +1,13 @@
 class Drucker:
-    Marke: str 
-    Farbe: str
+    marke: str 
+    farbe: str
     max_papier_drucken: int
     min_papier_drucken: int
     aktuell_papier_drucken: int
 
-    def __init__(self, Farbe, Marke) -> None:
-        self.marke = Marke 
-        self.farbe = Farbe 
+    def __init__(self, farbe, marke) -> None:
+        self.marke = marke 
+        self.farbe = farbe 
 
     def max_papier_drucken(self):
         self.aktuell_papier_drucken = self.max_papier_drucken
@@ -17,7 +17,6 @@ class Drucker:
 
 
 Drucker_von_Stefan = Drucker(Farbe = "weiß", Marke = "HP")
-Drucker_von_Stefan.anzahl_patronen = 4
 
 
 Drucker_von_Stefan.max_papier_drucken()
