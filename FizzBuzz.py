be420545bb253ec716cb3847e6b40b17a012ddc7
Stefan <115979341+xSTSNx@users.noreply.github.com>
2022-10-22 14:05:28 +0200

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -6,7 +6,7 @@ for x in range(1,101):
         print("FizzBuzz")
     elif (x % 3 == 0):
         print("Fizz")
-    elif (x % 5 == 0 ):
+    elif (x % 5 == 0):
         print("Buzz")
     else:
         print(x)
